b4cc346ff9ae94854ddd800b05d9ec0c5960eae3
shruthig-dev <68121218+shruthig-dev@users.noreply.github.com>
2020-08-24 09:01:46 -0700

    Update typewriter.js

--- typewriter.js
+++ typewriter.js
@@ -1,11 +1,10 @@
   
-    const sentence = "hello there from lighthouse labs\n";
-    let timeInterval = 50;
+const sentence = "hello there from lighthouse labs\n";
+let timeInterval = 50;
 
-    for(let i = 0; i< sentence.length;i++)
-    {
-        setTimeout(() => {
-            process.stdout.write (sentence[i]);
-          }, timeInterval+= 50)
-    }
+for (let i = 0; i < sentence.length; i++) {
+  setTimeout(() => {
+    process.stdout.write(sentence[i]);
+  }, timeInterval += 50);
+}
   
