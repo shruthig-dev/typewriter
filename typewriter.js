4271fabe3348de75d7dfa726fafd34620bc69f46
vagrant <shruthigangadharblr@gmail.com>
2020-08-24 15:57:37 +0000

    typewriter.js

---
+++ typewriter.js
@@ -0,0 +1,11 @@
+  
+    const sentence = "hello there from lighthouse labs\n";
+    let timeInterval = 50;
+
+    for(let i = 0; i< sentence.length;i++)
+    {
+        setTimeout(() => {
+            process.stdout.write (sentence[i]);
+          }, timeInterval+= 50)
+    }
+  
